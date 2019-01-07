bba60bb830fb530170a8d0135b53a47a2d316450
shinyscyther <mesibovj@gmail.com>
2019-01-07 15:26:46 -0500

    changing the html formatting to use forms and inputs that will later be used to create buttons from javascript instead of trying to have them be on the page from just the html

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,26 @@
+//establish the variable used to call my API Key
 var APIKey = "Yl5s8olf34BeRpqCqC56Qt4pkzStIr5N";
+//establish the variable that will be updated to reflect whatever the user types in the add button form.
+var query = "";
+//establish the query url that will be used for the ajax call that cancatonates the different necessary pieces of the URL
+var queryURL ="http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + APIKey;
+
+
+
+
+//make the ajax call within an onclick event for the appropriate button
+$(".GifBtn").on("click", function() {
+  //set the variable query = the text on the clicked button
+  query = this.
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(queryURL);
+  console.log(response);
+  //grab the appropriate gif (the first 10) and put it on the page
+
+})
+
+});
