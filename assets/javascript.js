ed252467c45a9fd4130f16aa7dbae290f0617b4b
shinyscyther <mesibovj@gmail.com>
2019-01-08 15:41:06 -0500

    added state change functionality to the gifs

--- assets/javascript.js
+++ assets/javascript.js
@@ -77,6 +77,20 @@ $(document).on("click", ".gif", function() {
   // read state of image to see if it's still or animated\
   // if still... animate
   // else... make still again
+  $(".gif").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 })
 
 // Calling the renderButtons function to display the intial buttons
